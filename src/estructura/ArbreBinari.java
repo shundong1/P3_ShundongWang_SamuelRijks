e2e62db53d1f1b955ee073623f87a03a1aac79ca
shundong1 <swang1@cat.tecnocampus.edu>
2023-11-09 18:22:03 +0100

    ArbreBinari y main

--- src/estructura/ArbreBinari.java
+++ src/estructura/ArbreBinari.java
@@ -1,5 +1,7 @@
 package estructura;
 
+import java.util.Scanner;
+
 public class ArbreBinari {
     private class Node<T> {
         T contingut;
@@ -28,7 +30,7 @@ public class ArbreBinari {
         if(node!=null){
 
             if(i==ronda){
-                System.out.println("Ronda2"+ronda+": ");
+                System.out.println("Ronda"+ronda+": ");
             }
             mostrarRecursivament(node.esq,i+1,ronda);
             mostrarRecursivament(node.dret,i+1,ronda);
@@ -48,4 +50,52 @@ public class ArbreBinari {
             introduirResultatsRecursivament(node.dret, nivell + 1, ronda, guanyador); // 递归右子树
         }
     }
+    //插入Equip节点
+    public void insert(Equip equip){
+        arrel = inserirRecursivament(arrel, equip);
+    }
+
+    private Node<Equip> inserirRecursivament(Node<Equip> arrel, Equip equip) {
+        if(arrel==null){
+            arrel = new Node<Equip>(equip);
+        }
+        if(equip.compareTo(arrel.contingut)<0){
+            arrel.esq=inserirRecursivament(arrel.esq,equip);
+        }else if(equip.compareTo(arrel.contingut)>0){
+            arrel.dret=inserirRecursivament(arrel.dret,equip);
+        }
+        return arrel;
+    }
+    // 创建锦标赛
+    public void crearTorneig(String nomTorneig, String[] nombresEquipos, int rounds) {
+        int profunditat = calcularProfunditat(nombresEquipos.length);
+        crearArbre(nombresEquipos, profunditat);
+
+        System.out.println("锦标赛创建成功！");
+    }
+
+
+    // 计算树的深度
+    private int calcularProfunditat(int numEquipos) {
+        int profunditat = 0;
+        int potencia = 1;
+
+        while (potencia < numEquipos) {
+            potencia *= 2;
+            profunditat++;
+        }
+
+        return profunditat + 1;
+    }
+
+    // 创建适当尺寸的树
+    private void crearArbre(String[] nombresEquipos, int profunditat) {
+        arrel = null;
+
+        for (String nombreEquipo : nombresEquipos) {
+            Equip equipo = new Equip(nombreEquipo.trim());
+            arrel = inserirRecursivament(arrel, equipo);
+        }
+    }
+
 }
--- src/execucio/Main.java
+++ src/execucio/Main.java
@@ -2,22 +2,57 @@ package execucio;
 
 import estructura.ArbreBinari;
 import estructura.Equip;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        ArbreBinari arbre = new ArbreBinari();
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+        while (choice != 1 && choice != 2) {
+            System.out.println("Seleccione la operación:");
+            System.out.println("1. Crear un nuevo torneo");
+            System.out.println("2. Gestionar un torneo existente");
+            choice = scanner.nextInt();
 
-            ArbreBinari arbre = new ArbreBinari();
-
-            // 创建一些队伍节点
-            Equip equip1 = new Equip("Equip 1");
-            Equip equip2 = new Equip("Equip 2");
-            Equip equip3 = new Equip("Equip 3");
-            Equip equip4 = new Equip("Equip 4");
-
-
-
-            // 显示整个树的内容
-            System.out.println("整个树的内容：");
-            arbre.mostrar();
+            if (choice != 1 && choice != 2) {
+                System.out.println("Las opciones son sólo 1 ó 2. Por favor, seleccione de nuevo.");
+            }
         }
+
+        if (choice == 1) {
+            // 创建新的锦标赛
+            System.out.println("Introduzca el nombre del torneo (que se convertirá en el nombre del archivo):");
+            String nomTorneig = scanner.next();
+
+            System.out.println("Introduzca los nombres de todos los equipos, separados por punto y coma:");
+            String input = scanner.next();
+            String[] nombresEquipos = input.split(";");
+
+            System.out.println("Introduzca el número de rondas:");
+            int rounds = scanner.nextInt();
+
+            arbre.crearTorneig(nomTorneig, nombresEquipos, rounds);
+        } else if (choice == 2) {
+            // 从文件中读取二叉树
+            // TODO: 添加从文件读取的逻辑，可以调用相应的方法
+        } else {
+            System.out.println("Las opciones son sólo 1 ó 2.");
+            return;
+        }
+
+        // 显示当前轮次
+        System.out.println("请输入当前轮次：");
+        int currentRound = scanner.nextInt();
+        arbre.mostrar(currentRound);
+
+        // 输入当前轮次的比赛结果
+        System.out.println("请输入比赛结果：");
+        // TODO: 添加输入比赛结果的逻辑，可以调用相应的方法
+        String winnerName = scanner.next();
+        Equip guanyador = new Equip(winnerName);
+        arbre.introduirResultats(currentRound, guanyador);
+
+        // 最终保存树到文件
+        // TODO: 添加保存树到文件的逻辑，可以调用相应的方法
     }
+}
